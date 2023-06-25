3684ba888b9a4e4de4fe8764b67f9fbdd8ff33c3
chanyoung0809 <cisalive@naver.com>
2023-06-26 08:44:37 +0900

    25일자 수정본

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require("express");
+const express = require("express"); // 익스프레스
+const multer  = require('multer');  // 파일업로드용 멀터
 const MongoClient = require("mongodb").MongoClient;
 //데이터베이스의 데이터 입력,출력을 위한 함수명령어 불러들이는 작업
 const app = express();
@@ -11,6 +12,38 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json()) 
 //css/img/js(정적인 파일)사용하려면 이코드를 작성!
 app.use(express.static('public'));
+
+// 로그인 검정 및 세션 생성에 필요한 기능 사용
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+app.use(session({secret :'secret', resave : false, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+// 로그인시 검증 처리
+passport.use(new LocalStrategy({
+    usernameField:"memberid",
+    passwordField:"memberpass",
+    session:true,
+    },      //해당 name값은 아래 매개변수에 저장
+    function(userMail, userPW, done) {
+                    //회원정보 콜렉션에 저장된 아이디랑 입력한 아이디랑 같은지 체크                                 
+      db.collection("cUsers").findOne({ userMail:userMail }, function (err, user) {
+        if (err) { return done(err); } //아이디 체크 시 코드(작업 X)
+        if (!user) { return done(null, false); }  //아이디 체크 시 코드(작업 X)
+        //비밀번호 체크 여기서 user는 db에 저장된 아이디의 비번값
+        if (userPW == user.userPW) { // 비밀번호 체크 시 코드
+            // 저장된 비밀번호가, 유저가 입력한 비밀번호와 같으면 if
+            return done(null, user);
+          } else {
+            // 다르면 else
+            return done(null, false);
+          }
+      });
+    }
+));
 
 //데이터 베이스 연결작업
 let db; //데이터베이스 연결을 위한 변수세팅(변수의 이름은 자유롭게 지어도 됨)
@@ -29,6 +62,32 @@ MongoClient.connect("mongodb+srv://cisalive:cisaliveS2@cluster0.cjlsn98.mongodb.
 
 });
 
+// 파일 저장을 위한 multer 설정 구간
+const storage = multer.diskStorage({
+    // storage 상수
+    destination: function (req, file, cb) {
+        // 어디에 저장할 것인가?
+      cb(null, 'public/upload') //업로드 폴더 지정
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf8'))
+      // 영어가 아닌 다른 파일명 안깨지고 나오게 처리(궰쇊어 안나오게 해줌)
+    }
+  })
+  
+const upload = multer({ storage: storage })
+//upload는 위의 설정사항을 담은 변수(상수) 
+
+// 회원가입 페이지
+app.get("/join", (req,res)=>{
+    res.render("join");
+});
+
+// 성인인증 페이지(추후 첫 세션 접속시에만 등장하게 함)
+app.get("/validAdult",(req,res)=>{
+    res.render("validAdult");
+});
+
 // 메인 페이지
 app.get("/",(req,res)=>{
     db.collection("whiskey").find().toArray((err, whiskey)=>{
@@ -37,7 +96,7 @@ app.get("/",(req,res)=>{
             for(let i=0; i<recipe.length; i+=3){
                 recipes.push(recipe[i]);
             }
-            res.render("index", {whiskey:whiskey, recipes:recipes})
+            res.render("index", {whiskey:whiskey, recipes:recipes, login:req.user})
         });
     });
     
@@ -51,7 +110,7 @@ app.get("/collections",(req,res)=>{
 app.get("/product/:link", (req, res)=>{
     db.collection("whiskey").findOne({link:req.params.link}, (err, prdResult)=>{
         db.collection("recipe").find({whiskey : req.params.link}).toArray((err, result)=>{
-            res.render("detail.ejs", {data:prdResult, cocktails:result});
+            res.render("detail.ejs", {data:prdResult, cocktails:result, login:req.user});
         })
     })
 })
@@ -60,7 +119,7 @@ app.get("/product/:link", (req, res)=>{
 app.get("/recipes",(req,res)=>{
     db.collection("recipe").find().toArray((err, recipes)=>{
         db.collection("whiskey").find().toArray((err, whiskey)=>{
-            res.render("recipes",{recipes:recipes, whiskey:whiskey});
+            res.render("recipes",{recipes:recipes, whiskey:whiskey, login:req.user});
         })  
     })
 });
@@ -68,7 +127,7 @@ app.get("/recipes",(req,res)=>{
 app.get("/recipes/:link",(req,res)=>{
     db.collection("whiskey").find().toArray((err, whiskey)=>{
         db.collection("recipe").find({"whiskey":req.params.link}).toArray((err, recipes)=>{
-            res.render("recipes",{recipes:recipes, whiskey:whiskey});
+            res.render("recipes",{recipes:recipes, whiskey:whiskey, login:req.user});
         })
     })    
 });
@@ -76,7 +135,7 @@ app.get("/recipes/:link",(req,res)=>{
 app.get("/recipe/:link",(req,res)=>{
     db.collection("recipe").findOne({"link":req.params.link}, (err, recipe)=>{
         db.collection("recipe").find().toArray((err, allRecipes)=>{
-            res.render("recipeDetail",{recipe:recipe, allRecipes:allRecipes});
+            res.render("recipeDetail",{recipe:recipe, allRecipes:allRecipes, login:req.user});
         });
 
     })
@@ -94,49 +153,42 @@ app.get("/location",(req,res)=>{
             return 0;
         });
         db.collection("store").find().toArray((err, stores)=>{    
-            res.render("location.ejs", {cities:cities, citiesSort:citiesSort, stores: stores,test:"noselect"});
+            res.render("location.ejs", {cities:cities, citiesSort:citiesSort, stores: stores, login:req.user, test:"noselect"});
         })
     });
 });
 
-//매장명,지역검색 db요청
+/* 판매처 찾기 기능 - 지역명 / 매장,도로명,지번주소로 찾기 2개 */
+//지역명검색 db요청
 app.get("/storelocation",(req,res)=>{
-    // 지역명으로 검색한 경우
-   
-        db.collection("cities").find().toArray((err, cities)=>{    
-            let citiesSort = cities.slice(); //배열 복제본
-            citiesSort = citiesSort.sort((a, b)=>{
-                // 오름차순 정렬한 새로운 배열 생성
-                if(a.korName > b.korName) return 1;
-                if(a.korName < b.korName) return -1;
-                return 0;
-            });
-        //지역명으로 검색해서 데이터가 전달된 경우
-            if(req.query.storeLegion !== "noselect"){
-                db.collection("store").find({engName:req.query.storeLegion}).toArray((err,stores)=>{
-                    res.render("location.ejs",{
-                        cities:cities, 
-                        citiesSort:citiesSort,
-                        stores: stores,
-                        test:req.query.storeLegion
-                    })
-                })
-            }
-            //지역명 선택안한경우 -> 매장 목록 페이지 경로로 요청
-            else {
-                res.redirect("/location");
-            }
+    db.collection("cities").find().toArray((err, cities)=>{    
+        let citiesSort = cities.slice(); //배열 복제본
+        citiesSort = citiesSort.sort((a, b)=>{
+            // 오름차순 정렬한 새로운 배열 생성
+            if(a.korName > b.korName) return 1;
+            if(a.korName < b.korName) return -1;
+            return 0;
         });
-    
-    // 매장명으로 검색한 경우, 
-
-    // 입력값이 정의되었고, 해당 정규식을 통과하였을 경우
-   
-        // 정규표현식에 맞게 검색한 경우
-
+    //지역명으로 검색해서 데이터가 전달된 경우
+        if(req.query.storeLegion !== "noselect"){
+            db.collection("store").find({engName:req.query.storeLegion}).toArray((err,stores)=>{
+                res.render("location.ejs",{
+                    cities:cities, 
+                    citiesSort:citiesSort,
+                    stores: stores,
+                    test:req.query.storeLegion,
+                    login:req.user
+                })
+            })
+        }
+        //지역명 선택안한경우 -> 매장 목록 페이지 경로로 요청
+        else {
+            res.redirect("/location");
+        }
+    });
 })
 
-
+// 매장명 검색 DB요청
 app.get("/storename",(req,res)=>{
     let check = [
         {
@@ -176,7 +228,8 @@ app.get("/storename",(req,res)=>{
                     cities:cities, 
                     citiesSort:citiesSort,
                     stores: stores,
-                    test:stores[0].engName
+                    test:stores[0].engName,
+                    login:req.user
                 })
             }
             else {
@@ -184,7 +237,8 @@ app.get("/storename",(req,res)=>{
                     cities:cities, 
                     citiesSort:citiesSort,
                     stores: undefined,
-                    test: "noselect"
+                    test: "noselect",
+                    login:req.user
                 })
             }
         })
@@ -194,36 +248,67 @@ app.get("/storename",(req,res)=>{
 // 히스토리 페이지
 app.get("/history",(req,res)=>{
     db.collection("history").find().toArray((err, history)=>{
-        res.render("history", {history:history});
+        res.render("history", {history:history, login:req.user});
     })
 });
 // 양조장 페이지
 app.get("/craft",(req,res)=>{
     db.collection("craft").find().toArray((err, craft)=>{
-        res.render("craft", {craft:craft});
+        res.render("craft", {craft:craft, login:req.user});
     })
 });
 // 이용약관 페이지
 app.get("/terms",(req,res)=>{
-    res.render("terms");
-});
-
-// 회원전용 Qna 페이지
-app.get("/qna",(req,res)=>{
-    res.render("QnA");
-});
-
-// Qna 상세 글 페이지
-app.get("/qna/detail/0",(req,res)=>{
-    res.render("QnaDetail");
+    res.render("terms", {login:req.user});
 });
 
 // 자주묻는 질문 페이지
 app.get("/faq",(req,res)=>{
-    res.render("faq");
+    res.render("faq", {login:req.user});
 });
 
-// 성인인증 페이지(추후 첫 세션 접속시에만 등장하게 함)
-app.get("/validAdult",(req,res)=>{
-    res.render("validAdult");
+// 회원전용 Qna 페이지 - Qna 모두보기
+// (일반 회원 -  자기가 쓴 글만 보임, 어드민 - 모든 글이 보임)
+// 답변 여부도 변수에 저장해야 함.
+app.get("/qna",(req,res)=>{
+    res.render("QnA", {login:req.user});
+});
+
+// Qna 상세 글 페이지 - 공지사항
+app.get("/qna/detail/notice",(req,res)=>{
+    res.render("QnaDetail", {login:req.user});
+});
+
+//qna 질문하기 페이지
+app.get("/qna/q_insert", (req, res)=>{
+    res.render("QuestionSubmit", {login:req.user});
+});
+
+//Qna 질문하기 DB 등록 작업
+app.post("/Qsubmit",upload.array("QnAs"),(req,res)=>{
+    let fileNames = [];
+    if(req.files){ // 첨부파일이 있다면
+        for(let i = 0; i < req.files.length; i++){
+            fileNames[i] = req.files[i].filename;
+            // 첨부한 파일들의 파일명만 뽑아서 배열에 옮겨담음
+        }
+    }
+    else {
+        // 첨부파일이 없어도 게시글은 등록되어야 함
+    }
+    db.collection("count").findOne({name:"qna갯수"},(err,countResult)=>{
+        db.collection("QnAs").insertOne({
+            Q_num:countResult.qnaCount,
+            Q_title:req.body.Q_title,
+            Q_author:req.body.Q_author,
+            Q_date:req.body.Q_date,
+            Q_context:req.body.Q_context,
+            attachfile:fileNames,
+            // 옮겨담은 배열명으로 첨부파일 수정
+        },(err,result)=>{
+            db.collection("count").updateOne({name:"qna갯수"},{$inc:{qnaCount:1}},(err,result)=>{
+                res.redirect(`/qna/detail/${countResult.qnaCount}`);
+            })
+        })
+    })
 });
---
+++ public/js/question.js
@@ -0,0 +1,73 @@
+const q_submit = document.querySelector("#q_submit"); // 폼태그
+const Q_title = document.querySelector("#Q_title"); // 제목
+const Q_author = document.querySelector("#Q_author"); // 작성자
+const Q_date = document.querySelector("#Q_date"); // 작성날짜
+const Q_file = document.querySelector("#Q_file"); // 첨부파일
+const Q_context = document.querySelector("#Q_context"); // 질문내용
+const Q_submit_btn = document.querySelector("#Q_submit_btn"); // 전송버튼
+const extCheck = [".jpg", ".jpeg", ".png", ".gif", ".webp"]; // 체크할 확장자명
+
+// 첨부파일이 n개 -> 첨부한 파일 중 하나라도 체크할 확장자명이 아니라면 업로드 막아야 함
+let validRequest = false; // 파일의 확장자명이 이미지인지 유효성검사용 변수
+// true일 때 데이터 전송 처리할 역할
+let validCount = 0; //체크 시 이미지 파일인 경우에만 카운트가 1씩 증가
+
+Q_submit_btn.addEventListener("click", (e)=>{
+    // 첨부파일이 있는 경우에만, 첨부파일 갯수만큼 반복문 처리
+    if(Q_file.files && Q_file.files.length > 0){ 
+        for(let i=0; i< Q_file.files.length; i++){
+
+            let fileName = Q_file.files[i].name; // i번째 파일명 가져오기
+            
+            let fileLength = fileName.length; // 파일명 길이값(나중에 확장자명 식별할 때 사용)
+    
+            let fileDots = fileName.lastIndexOf("."); // .기호가 시작하는 순번
+    
+            let fileExts = fileName.substring(fileDots, fileLength); // 추출한 확장자
+            
+            let fileChange = fileExts.toLowerCase(); // 확장자 소문자로 변경
+    
+            let result = extCheck.includes(fileChange); //해당 확장명이 위 배열안에 있는지?
+    
+            if(result){ // 파일이 올바른 이미지 확장명인지를 확인
+                validCount++;
+                if(validCount === inputFile.files.length){
+                    validRequest = true;
+                }
+            }
+        }
+        if(validRequest){
+            // validRequest 값이 true 라면
+            submitAuthor(); // 작성자와
+            submitDate(); // 작성날짜 input 태그에 삽입
+            q_submit.submit(); //업로드 기능 수행
+        }
+        else{
+            // validRequest 값이 false 라면
+            e.preventDefault(); // 업로드 기능막고
+            validCount = 0; //카운트 숫자 초기화
+            alert("이미지 파일만 업로드 가능합니다."); // 경고창
+        }
+    }
+    else { //파일을 첨부하지 않은 경우
+        submitAuthor(); // 작성자와
+        submitDate(); // 작성날짜 input 태그에 삽입
+        q_submit.submit(); //업로드 기능 수행
+    }
+});
+
+
+
+let submitAuthor = () => { // 글 작성자 삽입 함수
+    Q_author.value = login.userId;
+}
+
+let submitDate = ()=>{ // 글 작성 날짜 삽입 위한 함수
+    let date = new Date(); // Date 생성
+    let year = date.getFullYear(); // 작성연도
+    let month = ("0" + (date.getMonth() + 1)).slice(-2); // 작성월(1~9 : 01~09 / 10~12 : 010~012, slice로 뒤부터 2글자 잘라냄)
+    let day = ("0" + date.getDate()).slice(-2); // 작성일(1~9 : 01~09 / 10~31 : 010~031, slice로 뒤부터 2글자 잘라냄)
+    let writeDate = year + "-" + month + "-" + day; // 최종 삽입할 작성연월일 (yyyy-mm-dd 형식)
+    
+    Q_date.value = writeDate; //작성일자 인풋 태그 값으로 작성일자 값 저장.
+}
